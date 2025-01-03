8f913566ced88425269c128763bcd6a649782abc
illiaboiko <illiaboiko.095@gmail.com>
2025-01-03 17:27:11 +0200

    completed exercises 3.19 and 3.21 (without 3.20)

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    console.log("this was validation error");
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
@@ -71,7 +74,7 @@ app.delete("/api/persons/:id", (request, response, next) => {
 //   return maxId + 1;
 // };
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
   const { name, number } = body;
 
@@ -88,7 +91,8 @@ app.post("/api/persons", (request, response) => {
 
   person.save().then((savedPerson) => {
     response.json(savedPerson);
-  });
+  })
+  .catch(error => next(error));
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
@@ -96,15 +100,14 @@ app.put("/api/persons/:id", (request, response, next) => {
 
   const person = {
     name: body.name,
-    number: body.number
-  }
+    number: body.number,
+  };
 
-  Person.findByIdAndUpdate(request.params.id, person, {new: true})
-    .then(updatedPerson => {
-      response.json(updatedPerson)
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
     })
-    .catch(error => next(error))
-
+    .catch((error) => next(error));
 });
 
 app.use(unknownEndpoint);
