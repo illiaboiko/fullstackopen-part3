2ec52bc420e3835191d97d9690f818697d0b24a3
illiaboiko <illiaboiko.095@gmail.com>
2025-01-03 14:53:41 +0200

    completed exercises 3.15 - 3.18

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const morgan = require("morgan");
 const express = require("express");
-const cors = require("cors");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 
 const Person = require("./models/person");
 
 app.use(express.static("dist"));
 app.use(cors());
+app.use(express.json());
 
-let persons = [];
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
-app.use(express.json());
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
@@ -44,27 +52,24 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then( person => {
-    response.json(person)
-  })
-});
-
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    persons = persons.filter((p) => p.id !== id);
-    console.log("deleted successfully");
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(204).end();
-  }
+  });
 });
 
-const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      // response.status(204).end()
+      response.json(result);
+    })
+    .catch((error) => next(error));
+});
+
+// const generateId = () => {
+//   const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+//   return maxId + 1;
+// };
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
@@ -75,24 +80,35 @@ app.post("/api/persons", (request, response) => {
       error: "name and number must be specified",
     });
   }
-  // } else if (persons.find((p) => p.name === name)) {
-  //   return response.status(400).json({
-  //     error: "name must be unique",
-  //   });
-  // }
 
-  const person = new Person ({
+  const person = new Person({
     name: name,
     number: number,
-  })
+  });
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
-  
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+
 });
 
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
